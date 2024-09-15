072d5726b93dde75f1a8ed9d8d09a718c40d71e2
dolbin-prime <lunarlsb@gmail.com>
2024-09-15 14:26:05 +0900

    add empty App.css

--- counter/src/index.js
+++ counter/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
-import '@babel/polyfill';
+import App from './App.jsx';
 
 
 const container = document.getElementById('root');
