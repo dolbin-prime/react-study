7535308abf3fb156b7f54419b2a7b22e3df52d55
dolbin-prime <lunarlsb@gmail.com>
2024-09-17 11:06:09 +0900

    initial commit for stack-overflow

---
+++ stack-overflow/webpack.config.js
@@ -0,0 +1,50 @@
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import { CleanWebpackPlugin } from "clean-webpack-plugin";
+import path from "path";
+
+const __dirname = path.resolve();
+
+export default  {
+    mode : "development",
+    entry : "./src/index.js",
+    output : {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[fullhash].js",
+        publicPath: "/",
+    },
+    resolve : {
+        extensions: [".js", ".jsx", ".css"],
+    },
+    module : {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                // exclude: "/node_modules/",
+                loader: "babel-loader",
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: "style-loader",
+                    },
+                    {
+                        loader: "css-loader",
+                    },
+                ],
+            },
+        ],
+    },
+    plugins : [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: "./public/index.html",
+        }),
+    ],
+    devServer : {
+        host: "localhost",
+        port: 3000,
+        hot: true,
+        open: true,
+    },
+};
