a2daee707cc20e8ab70a8a291a07bf1f8b2e80cf
dolbin-prime <lunarlsb@gmail.com>
2024-09-17 16:14:39 +0900

    initial commit for todos

---
+++ todos/webpack.config.js
@@ -0,0 +1,49 @@
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import { CleanWebpackPlugin } from "clean-webpack-plugin";
+import path from "path";
+
+const dirname = path.resolve();
+
+export default  {
+    mode : "development",
+    entry : "./src/index.jsx",
+    output : {
+        path: path.resolve(dirname, "dist"),
+        filename: "[fullhash].js",
+        publicPath: "/",
+    },
+    resolve : {
+        extensions: [".js", ".jsx", ".css"],
+    },
+    module : {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                loader: "babel-loader",
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: "style-loader",
+                    },
+                    {
+                        loader: "css-loader",
+                    },
+                ],
+            },
+        ],
+    },
+    plugins : [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: "./public/index.html",
+        }),
+    ],
+    devServer : {
+        host: "localhost",
+        port: 3000,
+        hot: true,
+        open: true,
+    },
+};
